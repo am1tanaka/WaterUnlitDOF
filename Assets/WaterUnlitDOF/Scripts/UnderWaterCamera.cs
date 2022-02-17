7d8b440df6e695748a0f3473c29117583162c42e
Yu Tanaka <YRK00337@nifty.com>
2022-02-18 00:37:37 +0900

    水面下カメラでエラー中

---
+++ Assets/WaterUnlitDOF/Scripts/UnderWaterCamera.cs
@@ -0,0 +1,91 @@
+using UnityEngine;
+using UnityEngine.Rendering;
+
+[ExecuteInEditMode]
+public class UnderWaterCamera : MonoBehaviour
+{
+    /// <summary>
+    /// RenderTextureのサイズ
+    /// </summary>
+    static int renderTextureSize => 512;
+
+    /// <summary>
+    /// ビット数
+    /// </summary>
+    static int renderTextureDepth => 24;
+
+    Camera sourceCamera;
+    Camera underWaterCamera;
+    RenderTexture underWaterTex;
+    float currentAspect;
+
+    private void Awake()
+    {
+        sourceCamera = transform.parent.GetComponent<Camera>();
+        underWaterCamera = GetComponent<Camera>();
+        underWaterCamera.CopyFrom(sourceCamera);
+        underWaterCamera.cullingMask = underWaterCamera.cullingMask & (-1 ^ LayerMask.GetMask("Water"));
+        underWaterCamera.clearFlags = CameraClearFlags.Depth;
+        underWaterCamera.depth = -100;
+        underWaterCamera.depthTextureMode = DepthTextureMode.Depth;
+        UpdateRenderTex();
+    }
+
+    private void LateUpdate()
+    {
+        UpdateRenderTex();
+        Shader.SetGlobalTexture("_WaterDepthTex", underWaterTex);
+    }
+
+    private void OnDestroy()
+    {
+        DestroyTexture();
+    }
+
+    void DestroyTexture()
+    {
+        if (underWaterCamera.targetTexture != null)
+        {
+            underWaterCamera.targetTexture = null;
+        }
+        if (underWaterTex != null)
+        {
+            DestroyImmediate(underWaterTex);
+            underWaterTex = null;
+        }
+    }
+
+    /// <summary>
+    /// RenderTextureを生成
+    /// </summary>
+    void UpdateRenderTex()
+    {
+        if ((sourceCamera == null) || (underWaterCamera == null)) return;
+
+        if (underWaterTex != null)
+        {
+            if (currentAspect != sourceCamera.aspect)
+            {
+                currentAspect = sourceCamera.aspect;
+                DestroyTexture();
+            }
+            else
+            {
+                return;
+            }
+        }
+        underWaterTex = new RenderTexture(
+            renderTextureSize, renderTextureSize,
+            renderTextureDepth,
+            RenderTextureFormat.Depth,
+            RenderTextureReadWrite.Linear);
+        underWaterTex.dimension = TextureDimension.Tex2D;
+        underWaterTex.autoGenerateMips = false;
+        underWaterTex.anisoLevel = 1;
+        underWaterTex.filterMode = FilterMode.Point;
+        underWaterTex.wrapMode = TextureWrapMode.Clamp;
+        underWaterCamera.aspect = sourceCamera.aspect;
+        underWaterCamera.targetTexture = underWaterTex;
+        currentAspect = sourceCamera.aspect;
+    }
+}
